59c73c410443fc6212ad38d9c613212f14ac7f99
dyllan van den heever <dyllanvdhpf@gmail.com>
2022-06-09 16:49:38 +0200

    second commit

---
+++ dynamic-table-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from "./dynamic-table/DynamicTable";
+
+
+function App() {
+
+  return (
+    <DynamicTable />
+  );
+}
+
+export default App;
